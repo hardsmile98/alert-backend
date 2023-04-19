7873ffcd4072da3836c33035c0acb87a94948b46
Денис Котелев <deniskotelev@MacBook-Pro-Denis.local>
2023-04-19 22:44:44 +0300

    feat: limit

--- src/monitor/monitor.service.ts
+++ src/monitor/monitor.service.ts
@@ -1,4 +1,8 @@
-import { ForbiddenException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AddMonitorDto } from './dto';
@@ -7,12 +11,35 @@ import { AddMonitorDto } from './dto';
 export class MonitorService {
   constructor(private prisma: PrismaService) {}
 
-  async getMonitors(user: User) {
-    return await this.prisma.monitor.findMany({
+  async checkLimit(user: User) {
+    const countMonitors = await this.prisma.monitor.count({
       where: {
         userId: user.id,
       },
     });
+
+    if (user.plan === 'free') {
+      if (countMonitors === 1) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  async getMonitors(user: User) {
+    const monitors = await this.prisma.monitor.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    const isLimit = await this.checkLimit(user);
+
+    return {
+      monitors,
+      isLimit,
+    };
   }
 
   async deleteMonitor(user: User, id: number) {
@@ -59,6 +86,12 @@ export class MonitorService {
   }
 
   async addMonitor(user: User, dto: AddMonitorDto) {
+    const isLimit = await this.checkLimit(user);
+
+    if (isLimit) {
+      throw new BadRequestException('Limit monitors for plan');
+    }
+
     const { name, url, frequency } = dto;
 
     await this.prisma.monitor.create({
